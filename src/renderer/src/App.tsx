75621a80fb701eb85066a7b1b4ff6c50d26f0159
Farid Mahmudov <Razormad666@gmail.com>
2024-08-15 16:30:39 +0300

    update

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
+import { cn } from "./utils"
 
 function App(): JSX.Element {
 
+  const [theme, setTheme] = useState('light')
 
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  
+  const cssClasses = cn(theme === 'dark' ? 'app' : 'bg-slate-100', 'flex h-full justify-center items-center')
   return (
-    <>
-      <h1 className="text-black">Hello</h1>
-    </>
+
+    <div className={cssClasses}>
+      <h1 className="text-blue-500 text-3xl">Hello</h1>
+
+
+      <button onClick={() => toggleTheme()}>Toggle theme</button>
+    </div>
   )
 }
 
